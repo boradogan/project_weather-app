b7ff58f27a7d03bf4720f391117b31e3b984ff74
boradogan <doan.bora@gmail.com>
2025-07-26 20:31:16 +0300

    scaffolding

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 
 console.log('Selamun Aleykum Dunya')
+import { londonWeather } from "./mockData";
+
+const apiTest = () => {
+    const cityName = "london";
+    const API_KEY = "JJ6WZBUZTTL8NLURYSBYC9FTF";
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityName}?unitGroup=metric&key=${API_KEY}&contentType=json`;
+    console.log(url);
+    console.log(londonWeather);
+
+
+
+
+}
+
+apiTest();
